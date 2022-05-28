d31c2066a50b997ca4a4d9c5abefb67ba66e5ab9
Janainamc520 <janainamc520@gmail.com>
2022-05-28 15:40:25 -0300

    interface

---
+++ classes_objetos/interface.php
@@ -0,0 +1,26 @@
+<div class="titulo">Interface</div>
+<?php
+interface Animal{
+    function respirar();
+}
+
+interface Canino{
+    function latir(): string;
+}
+
+class Cachorro implements Animal, Canino{
+    function respirar()
+    {
+        return "Irei usar oxigênio!";
+    }
+
+    function latir(): string
+    {
+        return 'au au';
+    }
+}
+$animal1 = new Cachorro();
+echo $animal1->respirar(),'<br>';
+echo $animal1->latir(),'<br>';
+
+echo 'Fim!';
